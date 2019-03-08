d216ef4a50fdc03ba49a037b5769caaa3f366a7a
Hitham Ramzy <hitham.ramzy@mondiamedia.com>
2019-03-08 20:29:24 +0200

    Adding BalancedParentheses practice

---
+++ src/BalancedParentheses.java
@@ -0,0 +1,37 @@
+import java.util.Stack;
+
+public class BalancedParentheses {
+
+    private static Stack<Character> charsStack = new Stack<>();
+    private static final char[][] BRACKETS = {{'[', ']'}, {'(', ')'}, {'{', '}'}};
+
+    public static boolean testString(String stringToTest) {
+
+        char[] charToTest = stringToTest.toCharArray();
+        for (char c : charToTest) {
+            if (isOpenTerm(c)) {
+                charsStack.push(c);
+            } else {
+                if (charsStack.empty() || !isCloseOf(charsStack.peek(), c)) {
+                    return false;
+                }
+                charsStack.pop();
+            }
+        }
+        return true;
+    }
+
+    private static boolean isOpenTerm(char c) {
+        for (char[] arr : BRACKETS) {
+            if (arr[0] == c) return true;
+        }
+        return false;
+    }
+
+    private static boolean isCloseOf(char openTerm, char closeTerm) {
+        for (char[] arr : BRACKETS) {
+            if (arr[0] == openTerm) return arr[1] == closeTerm;
+        }
+        return false;
+    }
+}
