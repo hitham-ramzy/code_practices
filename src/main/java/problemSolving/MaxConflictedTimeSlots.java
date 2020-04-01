0ae504b0cddf0dcd9969024828e94b551829d623
hitham-ramzy <hitham.ramzy@gmail.com>
2020-04-01 22:38:52 +0200

    add merge sorter slotes to getConflictedTimeSlots problem solving

--- src/main/java/problemSolving/MaxConflictedTimeSlots.java
+++ src/main/java/problemSolving/MaxConflictedTimeSlots.java
@@ -1,7 +1,7 @@
 package problemSolving;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,6 +20,10 @@ class MaxConflictedTimeSlots {
         public int compareTo(SlotValue o) {
             return (int) (this.value - o.value);
         }
+
+        public String toString() {
+            return "" + value;
+        }
     }
 
     static int howManyAgentsToAdd(long[] startArray, long[] endArray) {
@@ -29,11 +33,10 @@ class MaxConflictedTimeSlots {
         List<SlotValue> endList = Arrays.stream(endArray).mapToObj(operand -> new SlotValue(operand, false)).collect(Collectors.toList());
 
         // merging and sorting them
-        startList.addAll(endList);
-        Collections.sort(startList);
+        List<SlotValue> mergedSlots = merge(startList, endList);
 
         // if the tine started then add one and save the value if it max, if the time is close them minus one
-        for (Object o : startList) {
+        for (Object o : mergedSlots) {
             SlotValue slotValue = (SlotValue) o;
             if (slotValue.isStart) {
                 sum++;
@@ -46,4 +49,22 @@ class MaxConflictedTimeSlots {
         }
         return max;
     }
+
+    private static List<SlotValue> merge(List<SlotValue> slots1, List<SlotValue> slots2) {
+        int slot1Index = 0;
+        int slot2Index = 0;
+        int mergedArraySize = slots1.size() + slots2.size();
+        List<SlotValue> mergedList = new ArrayList<>(mergedArraySize);
+        for (int i = 0; i < mergedArraySize; i++) {
+            if ((slot1Index < slots1.size() && slots1.get(slot1Index).value < slots2.get(slot2Index).value) || slot2Index >= slots2.size()) {
+                mergedList.add(slots1.get(slot1Index));
+                slot1Index++;
+            } else {
+                mergedList.add(slots2.get(slot2Index));
+                slot2Index++;
+            }
+        }
+        return mergedList;
+    }
+
 }
--- src/test/java/problemSolving/MaxConflictedTimeSlotsTest.java
+++ src/test/java/problemSolving/MaxConflictedTimeSlotsTest.java
@@ -11,7 +11,7 @@ public class MaxConflictedTimeSlotsTest {
     @Test
     public void testConflictedSlots() {
         long[] startArray = {1481122000, 1481122000, 1481122030, new Date().getTime()};
-        long[] endArray = {1481122020, 1481122040, 1481122035, new Date().getTime() + 1000};
+        long[] endArray = {1481122020, 1481122035, 1481122040, new Date().getTime() + 1000};
         assertEquals(2, MaxConflictedTimeSlots.howManyAgentsToAdd(startArray, endArray));
     }
 }
