9aecb2e850b2a29a1617e420acc40f9d8795436c
hitham-ramzy <hitham.ramzy@gmail.com>
2021-03-20 13:18:14 +0200

    add StringPermutation

---
+++ src/test/java/problemSolving/StringPermutationTest.java
@@ -0,0 +1,12 @@
+package problemSolving;
+
+import org.junit.Test;
+
+public class StringPermutationTest {
+
+    @Test
+    public void test(){
+        StringPermutation.permutation("ABCDE");
+    }
+}
+
